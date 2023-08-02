059f3f06fabe7fd7f45f1da94b84609244027209
JavohirJalilov <javohirjaliloff@gmail.com>
2023-08-02 10:57:56 +0500

    query params, and form

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
 def hello():
+    data = request.values
+    print(data)
     return "Hello world"
+
+@app.route('/form')
+def query():
+    html = """
+    <form action="http://127.0.0.1:5000/">
+    <label>a:</label><br>
+    <input type="text"  name="a" value="0"><br>
+    <label>b:</label><br>
+    <input type="text" name="b" value="0"><br><br>
+    <input type="submit" value="Submit">
+    </form>
+    """
+    return html
 
 if __name__ == "__main__":
     app.run(debug=True)
